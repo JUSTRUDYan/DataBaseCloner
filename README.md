df1eb2f15b9fc63b21a953288a83859b856be71b
RUDYan <112593269+JUSTRUDYan@users.noreply.github.com>
2024-02-19 01:02:00 +0200

    комітик

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# DataBaseCloner
- Script that cloning data base instance into docker container
+## **DumpRecoverSh - Database Dump and Recovery Tool**
+
+DumpRecoverSh is a bash script designed to facilitate the process of creating, importing, and dropping PostgreSQL database dumps using Docker containers.
+
+### **Usage**
+
+### **`createdumpinstance`**
+
+Creates a new Docker container, imports a PostgreSQL database dump, and optionally cleans up the container afterward.
+
+```bash
+bashCopy code
+./dumpRecover.sh createdumpinstance [options] dump_file_path
+
+```
+
+**Options:**
+
+- **`n`**: Name of the Docker container (optional, default: postgres).
+- **`d`**: Name of the database (optional, default: postgres).
+- **`f`**: Dump file path (required).
+- **`s`**: Show import log (optional, default: false).
+- **`e`**: Exit from the container after the "\q" command in PostgreSQL (optional, default: true).
+
+### **`dropdumpinstance`**
+
+Removes an existing Docker container.
+
+```bash
+bashCopy code
+./dumpRecover.sh dropdumpinstance [options]
+
+```
+
+**Options:**
+
+- **`n`**: Name of the Docker container (optional, default: postgres).
+
+### **Examples**
+
+### Creating a Database Dump Instance
+
+```bash
+bashCopy code
+./dumpRecover.sh createdumpinstance -n mycontainer -d mydatabase -f /path/to/dump.sql -s
+
+```
+
+- Creates a Docker container named "mycontainer" with a PostgreSQL database named "mydatabase."
+- Imports the database dump located at "/path/to/dump.sql" with import log displayed.
+- Accesses the PostgreSQL shell inside the container.
+
+### Dropping a Database Dump Instance
+
+```bash
+bashCopy code
+./dumpRecover.sh dropdumpinstance -n mycontainer
+
+```
+
+- Removes the Docker container named "mycontainer."
+
+### **Additional Information**
+
+- Ensure that Docker is installed and accessible in your environment.
+- The script utilizes the official PostgreSQL Docker image.
+
+### **Options Details**
+
+- **`n` (Name of Docker container):** Specifies the name of the Docker container. If not provided, the default name is "postgres."
+- **`d` (Name of the database):** Specifies the name of the PostgreSQL database. If not provided, the default name is "postgres."
+- **`f` (Dump file path):** Specifies the path to the PostgreSQL database dump file. This option is required.
+- **`s` (Show import log):** If specified, the import log will be displayed. If not, the log will be suppressed. Default is false.
+- **`e` (Exit from container after "\q" command):** If specified, the container will exit after executing the "\q" command in PostgreSQL. If not, it will remain running. Default is true.
+
+### **Notes**
+
+- The script checks for valid dump file extensions, requiring the file to have a ".sql" extension.
+- PostgreSQL password is set to "postgres" within the Docker container.
+
+### **Author**
+
+DumpRecoverSh is developed by RUDYAN.
+ Discord: rudy_an
--- script.sh
+++ script.sh
@@ -1,102 +1,157 @@
 #!/bin/bash
 
+function check_variable {
+    variable_name="$1"
+    parameter_name="$2"
+
+    if [[ "$variable_name" == -* ]]; then
+        echo "Error: Variable \"$variable_name\" for flag $2 cannot start with a hyphen."
+        exit 1
+    fi
+}
+
 help_createdumpinstance() {
     echo "Usage: createdumpinstance [dump_file_path]"
-    echo "  -n Name of docker container"
-    echo "  -d Name of data base"
+    echo "  -n Name of Docker container (optional). Default: postgres"
+    echo "  -d Name of database (optional). Default: postgres"
+    echo "  -f Dump file path (required)"
+    echo "  -s Show import log (optional). Default: false"
+    echo "  -e Exit from container after \"\\q\" command in PostgreSQL (optional). Default: true"
 }
 
 help_dropdumpinstance() {
     echo "Usage: dropdumpinstance"
-    echo "  -n Name of docker container"
+    echo "  -n Name of Docker container (optional). Default: postgres"
+}
+
+cleanup() {
+    local container_name="$1"
+
+    if docker container ls --format '{{.Names}}' | grep -q "$container_name"; then
+        echo "Removing Docker container \"$container_name\"."
+        docker rm "$container_name" -f
+    fi
 }
 
 createdumpinstance() {
     local dump_path=""
     local container_name="postgres"
     local database_name="postgres"
+    local show_import_log=false
+    local exit_from_container=true
 
     while [ -n "$1" ]; do
         case "$1" in
-            -h) help_createdumpinstance
+            -h) 
+                help_createdumpinstance
                 exit 0 ;;
-            -d) database_name="$2"
+            -d) 
+                database_name="$2"
+                check_variable "$database_name" "-d"
                 shift ;;
-            -n) container_name="$2"
+            -n) 
+                container_name="$2"
+                check_variable "$container_name" "-n"
                 shift ;;
-            -f) dump_path="$2"
-                shift;;
-            --) shift
+            -f) 
+                dump_path="$2"
+                check_variable "$dump_path" "-f"
+                shift ;;
+            -s) 
+                show_import_log=true
+                ;;
+            -e) 
+                exit_from_container=false
+                ;;
+            --) 
+                shift
                 break ;;
-            *) echo "$1 is not an option";;
+            *) 
+                echo "$1 is not an option" 
+                exit 1
+                ;;
         esac
         shift
     done
 
-    echo "$($dump_path) 1"
-    echo "$($container_name) 2"
-    echo "$($database_name) 3"
-
     if [[ -z "$dump_path" ]]; then
+        echo "Error: Dump path is not provided."
         help_createdumpinstance
         exit 1
     fi
 
     if ! [[ "$dump_path" == *.sql ]]; then
-        echo "Dump $dump_path is not the appropriate type (.sql). Is it okay?"
-        read -p "[Y/n] " answer
-        if [[ "$answer" != "Y" && "$answer" != "y" && "$answer" != "yes" && "$answer" != "Yes" ]]; then
-            echo "Exiting..."
-            exit 1
-        fi
+        echo "Error: Dump $dump_path is not the appropriate type (.sql)."
+        help_createdumpinstance
+        exit 1
     fi
 
-    runContainer "$dump_path" "$container_name" "$database_name"
+    runContainer "$dump_path" "$container_name" "$database_name" "$show_import_log" "$exit_from_container"
 }
 
 runContainer() {
     local dump_path="$1"
     local container_name="$2"
     local database_name="$3"
+    local show_import_log="$4"
+    local exit_from_container="$5"
 
+    echo "Creating Docker container \"$container_name\"."
     docker run -d --rm --name "$container_name" -e POSTGRES_PASSWORD="postgres" postgres
 
-    while ! docker exec -it "$container_name" pg_isready -q -h localhost -U postgres; do
+    until docker exec -it "$container_name" pg_isready -q -h localhost -U postgres; do
         sleep 1
     done
 
+    echo "Creating database \"$database_name\"."
     docker exec -it "$container_name" createdb -U postgres "$database_name"
 
-    docker exec -i "$container_name" psql -U postgres -d "$database_name" < "$dump_path"
+    if [ "$show_import_log" = true ]; then
+        echo "Importing data with log from \"$dump_path\"."
+        docker exec -i "$container_name" psql -U postgres -d "$database_name" < "$dump_path"
+    else
+        echo "Importing data without log from \"$dump_path\"."
+        docker exec -i "$container_name" psql -U postgres -d "$database_name" < "$dump_path" > /dev/null 2>&1
+    fi
 
-    docker exec -it "$container_name" psql -U postgres
+    echo "Accessing PostgreSQL shell in container \"$container_name\"."
+    docker exec -it "$container_name" psql -U postgres -d "$database_name"
+
+    if [ "$exit_from_container" = true ]; then
+        cleanup "$container_name"
+    fi
 }
 
 dropdumpinstance() {
     local container_name="postgres"
 
-    while [[ $# -gt 0 ]]; do 
+    while [ -n "$1" ]; do
         case "$1" in
             -h)
                 help_dropdumpinstance
                 exit 1
                 ;;
-            -n)
+            -n) 
                 container_name="$2"
-                shift 2 ;;
-            *)
-                echo "Parameter \"$1\" does not exist!"
+                check_variable "$container_name" "-n"
+                shift ;;
+            --) 
+                shift
+                break ;;
+            *) 
+                echo "$1 is not an option" 
                 exit 1
                 ;;
         esac
+        shift
     done
 
     if ! docker container ls --format '{{.Names}}' | grep -q "$container_name"; then
-        echo "Docker container \"$container_name\" does not exist!"
+        echo "Error: Docker container \"$container_name\" does not exist!"
         exit 1
     fi
 
-    docker rm "$container_name" -f
+    cleanup "$container_name"
     exit 0
 }
 
